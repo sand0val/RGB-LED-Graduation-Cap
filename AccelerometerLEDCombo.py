4797160ee2449c80a2b27ea109464b547a84fbef
Johnathan Sandoval <johnathan.sandoval@yahoo.com>
2018-12-13 02:41:12 -0600

    Create AccelerometerLEDCombo.py
    Cleaned version of the code. Could do with future improvements for sure....

---
+++ AccelerometerLEDCombo.py
@@ -0,0 +1,481 @@
+"""Code used by me in the designing of the RGB LED graduation cap. The RGB strip 
+digital input was plugged in on GPIO 18 using the BCM setting. The math is a bit sporadic
+BUT it is tuneable in senstivity. Hat is embedded with a raspberry pi and connected to 24 leds total on 4 led strips. The goal here was to 
+"""
+
+import smbus
+import time
+from neopixel import *
+import argparse
+import RPi.GPIO as BCM
+import numpy as np
+import matplotlib as mpl
+import matplotlib.pyplot as plt
+import matplotlib.animation as animation
+import serial
+import platform
+import argparse
+import signal
+import sys
+##--------------------------------------------- ....--------------
+
+fig, ax = plt.subplots()
+line, = ax.plot(np.random.rand(10))
+ax.set_ylim(-30, 30)
+xdata, ydata = [0]*100, [0]*100
+
+def update(data):
+    line.set_ydata(data)
+    return line,
+
+def run(data):
+    global xdata, ydata
+    x,y = data
+    if (x == 0):
+        xdata = [0]*100
+        ydata = [0]*100
+    del xdata[0]
+    del ydata[0]
+    xdata.append(x)
+    ydata.append(y)
+    line.set_data(xdata, ydata)
+    return line,
+
+value = 0
+
+##--------------------------------------------------------------------------
+def destroy():
+    GPIO.output(RelayPin,GPIO.HIGH)
+    GPIO.cleanup()
+
+    # LED strip configuration:
+LED_COUNT      = 24      # Number of LED pixels.
+LED_PIN        = 18      # GPIO pin connected to the pixels (18 uses PWM!).
+#LED_PIN        = 10      # GPIO pin connected to the pixels (10 uses SPI /dev/spidev0.0).
+LED_FREQ_HZ    = 800000  # LED signal frequency in hertz (usually 800khz)
+LED_DMA        = 10      # DMA channel to use for generating signal (try 10)
+LED_BRIGHTNESS = 1     # Set to 0 for darkest and 255 for brightest
+LED_STRIP      = ws.WS2811_STRIP_RGB
+LEDS           = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24]
+LED_INVERT     = False   # True to invert the signal (when using NPN transistor level shift)
+LED_CHANNEL    = 0       # set to '1' for GPIOs 13, 19, 41, 45 or 53
+strip = Adafruit_NeoPixel(LED_COUNT, LED_PIN, LED_FREQ_HZ, LED_DMA, LED_INVERT, LED_BRIGHTNESS, LED_CHANNEL, LED_STRIP)
+light = [-5,6,-3,6,-1,6,1,6,3,6,5,6,6,5,6,3,6,1,6,-1,6,-3,6,-5,5,-6,3,-6,1,-6,-1,-6,-3,-6,-5,-6,-6,-5,-6,-3,-6,-1,-6,1,-6,3,-6,5]
+for i in range(len(light)):
+    light[i]= light[i]/6
+
+# Define functions which animate LEDs in various ways.
+
+def alloff(strip, color, wait_ms=5000):
+    for i in range(strip.numPixels()):
+        strip.setPixelColor(i,color)
+        strip.show()
+
+def lightsoff():
+    strip = Adafruit_NeoPixel(LED_COUNT, LED_PIN, LED_FREQ_HZ, LED_DMA, LED_INVERT, LED_BRIGHTNESS, LED_CHANNEL, LED_STRIP)
+    strip.begin()
+    alloff(strip,Color(0,0,0))
+
+
+def colorWipe(strip, color, wait_ms=50):
+    """Wipe color across display a pixel at a time."""
+    for i in range(strip.numPixels()):
+        strip.setPixelColor(i, color)
+        strip.show()
+        time.sleep(wait_ms/1000.0)
+
+def wheel(pos):
+    """Generate rainbow colors across 0-255 positions."""
+    if pos < 85:
+        return Color(pos * 3, 255 - pos * 3, 0)
+    elif pos < 170:
+        pos -= 85
+        return Color(255 - pos * 3, 0, pos * 3)
+    else:
+        pos -= 170
+        return Color(0, pos * 3, 255 - pos * 3)
+
+def rainbow(strip, wait_ms=20, iterations=1):
+    """Draw rainbow that fades across all pixels at once."""
+    for j in range(256*iterations):
+        for i in range(strip.numPixels()):
+            strip.setPixelColor(i, wheel((i+j) & 255))
+        strip.show()
+        time.sleep(wait_ms/1000.0)
+
+def rainbow2(strip, wait_ms=20, iterations=1):
+    """Draw rainbow that fades across all pixels at once."""
+    for j in range(256*iterations):
+        for i in range(strip.numPixels()):
+            strip.setPixelColor(i, wheel((i+j) & 255))
+        strip.show()
+        time.sleep(wait_ms/1000.0)
+
+def theaterChase(strip, color, wait_ms=50, iterations=10):
+    """Movie theater light style chaser animation."""
+    for j in range(iterations):
+        for q in range(3):
+            for i in range(0, strip.numPixels(), 3):
+                strip.setPixelColor(i+q, color)
+            strip.show()
+            time.sleep(wait_ms/1000.0)
+            for i in range(0, strip.numPixels(), 3):
+                strip.setPixelColor(i+q, 0)
+                
+def rainbowCycle(strip, wait_ms=20, iterations=5):
+    """Draw rainbow that uniformly distributes itself across all pixels."""
+    for j in range(256*iterations):
+        for i in range(strip.numPixels()):
+            strip.setPixelColor(i, wheel((int(i * 256 / strip.numPixels()) + j) & 255))
+        strip.show()
+        time.sleep(wait_ms/1000.0)
+
+def theaterChaseRainbow(strip, wait_ms=50):
+    """Rainbow movie theater light style chaser animation."""
+    for j in range(256):
+        for q in range(3):
+            for i in range(0, strip.numPixels(), 3):
+                strip.setPixelColor(i+q, wheel((i+j) % 255))
+            strip.show()
+            time.sleep(wait_ms/1000.0)
+            for i in range(0, strip.numPixels(), 3):
+                strip.setPixelColor(i+q, 0)
+
+def singlecolor(strip, color, wait_ms=200):
+    """Wipe color across display a pixel at a time."""
+    for i in range(strip.numPixels()):
+        strip.setPixelColor(i, color)
+        strip.show()
+        time.sleep(wait_ms/1000.0)
+
+##--------------------------------------------------------------------
+    
+class mpu6050:
+
+    # Global Variables
+    GRAVITIY_MS2 = 9.80665
+    address = None
+    bus = None
+
+    # Scale Modifiers
+    ACCEL_SCALE_MODIFIER_2G = 16384.0
+    ACCEL_SCALE_MODIFIER_4G = 8192.0
+    ACCEL_SCALE_MODIFIER_8G = 4096.0
+    ACCEL_SCALE_MODIFIER_16G = 2048.0
+
+    GYRO_SCALE_MODIFIER_250DEG = 131.0
+    GYRO_SCALE_MODIFIER_500DEG = 65.5
+    GYRO_SCALE_MODIFIER_1000DEG = 32.8
+    GYRO_SCALE_MODIFIER_2000DEG = 16.4
+
+    # Pre-defined ranges
+    ACCEL_RANGE_2G = 0x00
+    ACCEL_RANGE_4G = 0x08
+    ACCEL_RANGE_8G = 0x10
+    ACCEL_RANGE_16G = 0x18
+
+    GYRO_RANGE_250DEG = 0x00
+    GYRO_RANGE_500DEG = 0x08
+    GYRO_RANGE_1000DEG = 0x10
+    GYRO_RANGE_2000DEG = 0x18
+
+    # MPU-6050 Registers
+    PWR_MGMT_1 = 0x6B
+    PWR_MGMT_2 = 0x6C
+
+    ACCEL_XOUT0 = 0x3B
+    ACCEL_YOUT0 = 0x3D
+    ACCEL_ZOUT0 = 0x3F
+
+    TEMP_OUT0 = 0x41
+
+    GYRO_XOUT0 = 0x43
+    GYRO_YOUT0 = 0x45
+    GYRO_ZOUT0 = 0x47
+
+    ACCEL_CONFIG = 0x1C
+    GYRO_CONFIG = 0x1B
+
+    def __init__(self, address, bus=1):
+        self.address = address
+        self.bus = smbus.SMBus(bus)
+        # Wake up the MPU-6050 since it starts in sleep mode
+        self.bus.write_byte_data(self.address, self.PWR_MGMT_1, 0x00)
+
+    # I2C communication methods
+
+    def read_i2c_word(self, register):
+        """Read two i2c registers and combine them.
+
+        register -- the first register to read from.
+        Returns the combined read results.
+        """
+        # Read the data from the registers
+        high = self.bus.read_byte_data(self.address, register)
+        low = self.bus.read_byte_data(self.address, register + 1)
+
+        value = (high << 8) + low
+
+        if (value >= 0x8000):
+            return -((65535 - value) + 1)
+        else:
+            return value
+
+    # MPU-6050 Methods
+
+    def get_temp(self):
+        """Reads the temperature from the onboard temperature sensor of the MPU-6050.
+
+        Returns the temperature in degrees Celcius.
+        """
+        raw_temp = self.read_i2c_word(self.TEMP_OUT0)
+
+        # Get the actual temperature using the formule given in the
+        # MPU-6050 Register Map and Descriptions revision 4.2, page 30
+        actual_temp = (raw_temp / 340.0) + 36.53
+
+        return actual_temp
+
+    def set_accel_range(self, accel_range):
+        """Sets the range of the accelerometer to range.
+
+        accel_range -- the range to set the accelerometer to. Using a
+        pre-defined range is advised.
+        """
+        # First change it to 0x00 to make sure we write the correct value later
+        self.bus.write_byte_data(self.address, self.ACCEL_CONFIG, 0x00)
+
+        # Write the new range to the ACCEL_CONFIG register
+        self.bus.write_byte_data(self.address, self.ACCEL_CONFIG, accel_range)
+
+    def read_accel_range(self, raw = False):
+        """Reads the range the accelerometer is set to.
+
+        If raw is True, it will return the raw value from the ACCEL_CONFIG
+        register
+        If raw is False, it will return an integer: -1, 2, 4, 8 or 16. When it
+        returns -1 something went wrong.
+        """
+        raw_data = self.bus.read_byte_data(self.address, self.ACCEL_CONFIG)
+
+        if raw is True:
+            return raw_data
+        elif raw is False:
+            if raw_data == self.ACCEL_RANGE_2G:
+                return 2
+            elif raw_data == self.ACCEL_RANGE_4G:
+                return 4
+            elif raw_data == self.ACCEL_RANGE_8G:
+                return 8
+            elif raw_data == self.ACCEL_RANGE_16G:
+                return 16
+            else:
+                return -1
+
+    def get_accel_data(self, g = False):
+        """Gets and returns the X, Y and Z values from the accelerometer.
+
+        If g is True, it will return the data in g
+        If g is False, it will return the data in m/s^2
+        Returns a dictionary with the measurement results.
+        """
+        x = self.read_i2c_word(self.ACCEL_XOUT0)
+        y = self.read_i2c_word(self.ACCEL_YOUT0)
+        z = self.read_i2c_word(self.ACCEL_ZOUT0)
+
+        accel_scale_modifier = None
+        accel_range = self.read_accel_range(True)
+
+        if accel_range == self.ACCEL_RANGE_2G:
+            accel_scale_modifier = self.ACCEL_SCALE_MODIFIER_2G
+        elif accel_range == self.ACCEL_RANGE_4G:
+            accel_scale_modifier = self.ACCEL_SCALE_MODIFIER_4G
+        elif accel_range == self.ACCEL_RANGE_8G:
+            accel_scale_modifier = self.ACCEL_SCALE_MODIFIER_8G
+        elif accel_range == self.ACCEL_RANGE_16G:
+            accel_scale_modifier = self.ACCEL_SCALE_MODIFIER_16G
+        else:
+            print("Unkown range - accel_scale_modifier set to self.ACCEL_SCALE_MODIFIER_2G")
+            accel_scale_modifier = self.ACCEL_SCALE_MODIFIER_2G
+
+        x = x / accel_scale_modifier
+        y = y / accel_scale_modifier
+        z = z / accel_scale_modifier
+
+        if g is True:
+            return {'x': x, 'y': y, 'z': z}
+        elif g is False:
+            x = x * self.GRAVITIY_MS2
+            y = y * self.GRAVITIY_MS2
+            z = z * self.GRAVITIY_MS2
+            return {'x': x, 'y': y, 'z': z}
+
+    def set_gyro_range(self, gyro_range):
+        """Sets the range of the gyroscope to range.
+
+        gyro_range -- the range to set the gyroscope to. Using a pre-defined
+        range is advised.
+        """
+        # First change it to 0x00 to make sure we write the correct value later
+        self.bus.write_byte_data(self.address, self.GYRO_CONFIG, 0x00)
+
+        # Write the new range to the ACCEL_CONFIG register
+        self.bus.write_byte_data(self.address, self.GYRO_CONFIG, gyro_range)
+
+    def read_gyro_range(self, raw = False):
+        """Reads the range the gyroscope is set to.
+
+        If raw is True, it will return the raw value from the GYRO_CONFIG
+        register.
+        If raw is False, it will return 250, 500, 1000, 2000 or -1. If the
+        returned value is equal to -1 something went wrong.
+        """
+        raw_data = self.bus.read_byte_data(self.address, self.GYRO_CONFIG)
+
+        if raw is True:
+            return raw_data
+        elif raw is False:
+            if raw_data == self.GYRO_RANGE_250DEG:
+                return 250
+            elif raw_data == self.GYRO_RANGE_500DEG:
+                return 500
+            elif raw_data == self.GYRO_RANGE_1000DEG:
+                return 1000
+            elif raw_data == self.GYRO_RANGE_2000DEG:
+                return 2000
+            else:
+                return -1
+
+    def get_gyro_data(self):
+        """Gets and returns the X, Y and Z values from the gyroscope.
+
+        Returns the read values in a dictionary.
+        """
+        x = self.read_i2c_word(self.GYRO_XOUT0)
+        y = self.read_i2c_word(self.GYRO_YOUT0)
+        z = self.read_i2c_word(self.GYRO_ZOUT0)
+
+        gyro_scale_modifier = None
+        gyro_range = self.read_gyro_range(True)
+
+        if gyro_range == self.GYRO_RANGE_250DEG:
+            gyro_scale_modifier = self.GYRO_SCALE_MODIFIER_250DEG
+        elif gyro_range == self.GYRO_RANGE_500DEG:
+            gyro_scale_modifier = self.GYRO_SCALE_MODIFIER_500DEG
+        elif gyro_range == self.GYRO_RANGE_1000DEG:
+            gyro_scale_modifier = self.GYRO_SCALE_MODIFIER_1000DEG
+        elif gyro_range == self.GYRO_RANGE_2000DEG:
+            gyro_scale_modifier = self.GYRO_SCALE_MODIFIER_2000DEG
+        else:
+            print("Unkown range - gyro_scale_modifier set to self.GYRO_SCALE_MODIFIER_250DEG")
+            gyro_scale_modifier = self.GYRO_SCALE_MODIFIER_250DEG
+
+        x = x / gyro_scale_modifier
+        y = y / gyro_scale_modifier
+        z = z / gyro_scale_modifier
+
+        return {'x': x, 'y': y, 'z': z}
+
+    def get_all_data(self):
+        """Reads and returns all the available data."""
+        temp = self.get_temp()
+        accel = self.get_accel_data()
+        gyro = self.get_gyro_data()
+
+        return [accel, gyro, temp]
+
+       
+
+##-----------------------------------------------------------------
+
+    
+# Main program logic follows:
+if __name__ == '__main__':
+    # Process arguments
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-c', '--clear', action='store_true', help='clear the display on exit')
+    args = parser.parse_args()
+    led = np.zeros(24)
+    def litled(xa,ya,val):
+        for i in range(strip.numPixels()): #Determining how far a light is from point
+            if i == 0:
+                rax = light[i]
+                ray = light[i+1]
+                dx = rax-xa
+                dy = ray-ya
+                dist = np.sqrt(dx**(2)+dy**(2))
+                led[i] = round(dist,3)
+            else: 
+                rax = light[2*i-1]
+                ray = light[2*i]
+                dx = rax-xa
+                dy = ray-ya
+                dist = np.sqrt(dx**(2)+dy**(2))
+                led[i] = round(dist,3)
+            dist = round(dist,3)
+        minlist = round(min(led),0)
+        maxlist = round(max(led),0)
+        rang = round(maxlist-minlist,0)
+        new = led/maxlist #decimal percentage of how close it is to the "hotspot" where all readings are detected
+        brightness = np.round(new*176,0)
+        for j in range(strip.numPixels()): #finds the max area spot
+                colorbar = int(val)     # Set to 0 for darkest and 255 for brightest
+                strip.setPixelColor(j, wheel(colorbar))
+
+
+ 
+
+    cycles = 0   #Amount of cycles performed
+    nml =10 #normalizing turns
+    rxm = np.zeros(nml)  #X array of normalizing cycles/turns opening up
+    rxy = np.zeros(nml)
+    maxx = np.zeros(nml)  #X array of normalizing cycles/turns opening up
+    maxy = np.zeros(nml)
+    mpu = mpu6050(0x68)
+    print ('Press Ctrl-C to quit.')
+    if not args.clear:
+        print('Use "-c" argument to clear LEDs on exit')
+    # Create NeoPixel object with appropriate configuration.
+    strip = Adafruit_NeoPixel(LED_COUNT, LED_PIN, LED_FREQ_HZ, LED_DMA, LED_INVERT, LED_BRIGHTNESS, LED_CHANNEL)
+    # Intialize the library (must be called once before other functions).
+    strip.begin()
+
+    try:
+
+        while cycles < 12000:
+            
+            for k in range(nml):
+                maxx = 0
+                maxy = 0
+                switch = 0
+                time.sleep(0.001)
+                accel_data = mpu.get_accel_data()
+                gyro_data = mpu.get_gyro_data()
+                rx, ry = gyro_data['x'],gyro_data['y']
+                x, y = accel_data['x'],accel_data['y']
+                rxm[k]=x
+                rxy[k]=y
+
+            rx = sum(rxm)/float(len(rxm)) #takes the average of x values
+            ry = sum(rxy)/float(len(rxy)) #takes the average of y values
+            valx = abs(int(round(rx,2)))
+            valy = abs(int(round(ry,2)))
+            value = np.sqrt(valx**(2) + valy**(2))
+            value = 13.1*value
+            print(value)
+            if value < 170:
+                value = value
+            else:
+                value = 170
+            litled(rx,ry,value)
+            #print('X=%s, Y = %s, value=%s' %(rx,ry, value))
+            strip.show()            
+            cycles = cycles+1
+            
+
+    except KeyboardInterrupt:
+        if args.clear:
+            lightsoff()
+            destroy()
+lightsoff()
+  
